d52662eb7124939e84c4cdba23bb0e31f5c4505f
Rob Rankin <rob@revolutionstrategy.com>
2018-07-18 10:55:50 -0300

    first commit

---
+++ src/ProviderLoginButtons.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RevStrat\OAuthButtons;
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Forms\HiddenField;
+use Bigfork\SilverStripeOAuth\Client\Authenticator\Authenticator;
+
+class ProviderLoginButtons extends DataExtension {
+    public function LoginButton($backURL = '/', $provider = NULL) {
+        $authenticator = Injector::inst()->get(Authenticator::class);
+        $handler = $authenticator->getLoginHandler($this->owner->Link());
+        $form = $handler->loginForm();
+
+        if ($provider) {
+            foreach($form->Actions() as $action) {
+                if ($action->Name !== "action_authenticate_$provider") {
+                    $form->Actions()->remove($action);
+                }
+            }
+        }
+        
+        $form->Fields()->replaceField('BackURL', HiddenField::create('BackURL', null, $backURL));
+        return $form;
+    }
+}
