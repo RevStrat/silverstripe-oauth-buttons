c059eb1789e37a61557e577c985d7bdcff4b7d1b
Rob Rankin <rob@revolutionstrategy.com>
2018-07-18 14:21:54 -0300

    learn to love the lack of backurl control

--- src/ProviderLoginButtons.php
+++ src/ProviderLoginButtons.php
@@ -2,15 +2,20 @@
 
 namespace RevStrat\OAuthButtons;
 
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\Director;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Forms\HiddenField;
 use Bigfork\SilverStripeOAuth\Client\Authenticator\Authenticator;
 
+use SilverStripe\Control\HTTPRequest;
+
 class ProviderLoginButtons extends DataExtension {
     public function LoginButton($backURL = '/', $provider = NULL) {
+        
         $authenticator = Injector::inst()->get(Authenticator::class);
-        $handler = $authenticator->getLoginHandler('/Security/login/oauth/');
+        $handler = $authenticator->getLoginHandler(Director::baseURL() . 'Security/login/oauth/');
         $form = $handler->loginForm();
 
         if ($provider) {
@@ -20,8 +25,7 @@ class ProviderLoginButtons extends DataExtension {
                 }
             }
         }
-        
-        $form->Fields()->replaceField('BackURL', HiddenField::create('BackURL', null, $backURL));
+
         return $form;
     }
 }
