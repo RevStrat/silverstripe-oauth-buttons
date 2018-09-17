e9eec0464ca3c99e784391f707acc82d666798db
Rob Rankin <rob@revolutionstrategy.com>
2018-09-17 12:44:10 -0300

    Add namespacing

--- src/ProviderLoginButtons.php
+++ src/ProviderLoginButtons.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace RevStrat\OAuthButtons;
+
 use Bigfork\SilverStripeOAuth\Client\Authenticator\Authenticator as OAuthenticator;
 
 class ProviderLoginButtons extends DataExtension {
     public function LoginButton($provider = NULL, $textOverride = NULL) {
         $form =  OAuthenticator::get_login_form(Controller::curr());
-        $form->setHTMLID(uniqid('Auth_'));
+        //$form->setHTMLID(uniqid('Auth_'));
         $form->addExtraClass('OAuthAuthenticator');
         $backURLField = $form->HiddenFields()->fieldByName('BackURL');
         if ($backURLField) {
