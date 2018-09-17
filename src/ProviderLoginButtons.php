41e8ca9d0b3d193158ffd31f4024f9d1425ebf3f
Rob Rankin <rob@revolutionstrategy.com>
2018-09-17 13:24:53 -0300

    Fix missing namespace

--- src/ProviderLoginButtons.php
+++ src/ProviderLoginButtons.php
@@ -3,6 +3,7 @@
 namespace RevStrat\OAuthButtons;
 
 use Bigfork\SilverStripeOAuth\Client\Authenticator\Authenticator as OAuthenticator;
+use SilverStripe\ORM\DataExtension;
 
 class ProviderLoginButtons extends DataExtension {
     public function LoginButton($provider = NULL, $textOverride = NULL) {
