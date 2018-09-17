9617c0fee1d882be451449e97a62a78937fdb3d7
Rob Rankin <rob@revolutionstrategy.com>
2018-09-17 14:50:03 -0300

    Update for SS4 support

--- src/ProviderLoginButtons.php
+++ src/ProviderLoginButtons.php
@@ -7,7 +7,9 @@ use SilverStripe\ORM\DataExtension;
 
 class ProviderLoginButtons extends DataExtension {
     public function LoginButton($provider = NULL, $textOverride = NULL) {
-        $form =  OAuthenticator::get_login_form(Controller::curr());
+        $authenticator = new OAuthenticator();
+        $handler = $authenticator->getLoginHandler($this->owner->Link());
+        $form = $handler->loginForm();
         //$form->setHTMLID(uniqid('Auth_'));
         $form->addExtraClass('OAuthAuthenticator');
         $backURLField = $form->HiddenFields()->fieldByName('BackURL');
@@ -16,7 +18,7 @@ class ProviderLoginButtons extends DataExtension {
         } else {
             $form->HiddenFields()->add(HiddenField::create('BackURL', $this->owner->Link()));
         }
-        $form->setFormAction('/Security/LoginForm');
+        $form->setFormAction('/Security/login/oauth/LoginForm/');
         if ($provider) {
             foreach ($form->Actions() as $action) {
                 if ($action->Name !== "action_authenticate_$provider") {
